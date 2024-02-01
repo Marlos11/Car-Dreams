96acc14530af9880a2f17e1d4c3aad179ee936e0
Marlos11 <marlos-gama@hotmail.com>
2024-01-31 23:39:19 -0300

    correcao das funcoes de manipulacao de cores

--- index.js
+++ index.js
@@ -1,64 +1,74 @@
 const changColors = document.querySelectorAll('.colors-palette li span')
-const carSelected = document.querySelector('.section-car-img img ')
+const carSelected = document.querySelector('.section-car-img ')
 const carInfo = document.getElementById('info')
-const showCar = document.getElementsByClassName('.section-car-img')
-
-function changeInfo(marca){
-    if(marca === 'audi'){
-        carInfo.innerHTML = 'Com o audi rs e-tron gt a audi traduz a linguagem de design a era da eletricidade.'
-        carSelected.src = `./assets/audi-rs-etronGT-${colorSelected}.png`
-    
-
-      
-        
-    }else if(marca ==='ferrari'){
-        carInfo.innerHTML = 'Ferrari SF90 Stradale Consumo de combustível em ciclo combinado: 6,1 l/100 km - Consumo de energia em ciclo combinado: 123 kWh/100 km - Emissões de CO2 em ciclo combinado: 154 g/km - Classe de eficiência de CO2: C '
-        carSelected.src = `./assets/ferrari-sfo90-am.png`
-     
-    }else if(marca === 'tesla'){
-        carInfo.innerHTML = 'Construído para distância Vá até 341 milhas (est. EPA) com uma única carga com estilo exterior atualizado e otimizado para máxima aerodinâmica.'
-    } 
+const showCar = document.querySelector('.section-car-img')
+const carBrands = document.querySelectorAll('.carBrend') 
 
 
-   
 
-}
+let colorSelected = 'vm'
 
-let colorSelected='vm'
+carBrands.forEach(brand => {
+    brand.addEventListener('click', () => {
+        const marca = brand.dataset.marca
+     changeInfo(marca)
+        updateCar() 
+    })
+})
 
 changColors.forEach(li =>
     li.addEventListener('click', () => {
 
-       
+
         changColors.forEach(span => {
 
             span.classList.remove('selected')
-           
+
         })
 
-        li.classList.add('selected') 
+        li.classList.add('selected')
 
-         colorSelected = li.classList[0]
-     
-         handleShowCar()
-         showCarSelect()
-      
- 
+        colorSelected = li.classList[0]
+        carSelected.src = `./assets/audi-${colorSelected}.png`;
+  
+
     })
 )
 
-function handleShowCar(){
 
-    carSelected.src = `./assets/audi-rs-etronGT-${colorSelected}.png`
+function changeInfo(marca) {
 
-  
-  
+    if (marca === 'audi') {
+        carInfo.innerHTML = 'Com o audi rs e-tron gt a audi traduz a linguagem de design a era da eletricidade.'
+        carSelected.src = `./assets/audi-${colorSelected}.png`;
+
+    } else if (marca === 'ferrari') {
+        carInfo.innerHTML = 'Ferrari SF90 Stradale Consumo de combustível em ciclo combinado: 6,1 l/100 km - Consumo de energia em ciclo combinado: 123 kWh/100 km - Emissões de CO2 em ciclo combinado: 154 g/km - Classe de eficiência de CO2: C '
+      /*   carSelected.src = `./assets/ferrari-sfo90-${colorSelected}.png`; */
+
+    } else if (marca === 'tesla') {
+        carInfo.innerHTML = 'Construído para distância Vá até 341 milhas (est. EPA) com uma única carga com estilo exterior atualizado e otimizado para máxima aerodinâmica.'
+    }
 }
-function showCarSelect(){
-   carSelected.src=`${showCar}`
-  
-} 
- 
-console.log(changColors)
+
+
+
+function updateCar() {
+    const selectedBrand = document.querySelector('.carBrand.selected'); 
+    if (selectedBrand) {
+        const marca = selectedBrand.dataset.marca;
+         carSelected.src = `./assets/${marca}-${colorSelected}.png`;
+      
+       
+    }
+}
+
+
+
+
+
+
+
+
 
 
